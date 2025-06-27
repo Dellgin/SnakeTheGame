a385e971552110e55b2a7e94ddcc56cce2d7f15b
Rilus <dellgin@yandex.ru>
2025-06-27 21:22:38 +0400

    Отрисовка поля

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿public static class Game
+{
+    private static int _width = 48;
+    private static int _height = 24;
+
+    public static void Run()
+    {
+        Console.CursorVisible = false;
+        Console.Clear();
+        RenderField();
+    }
+
+    private static void RenderField()
+    {
+        Console.WriteLine(new string('#', _width));
+        for (int i = 1; i < _height - 1; i++)
+        {
+            Console.WriteLine($"#{new string(' ', _width - 2)}#");
+        }
+        Console.WriteLine(new string('#', _width));
+    }
+}

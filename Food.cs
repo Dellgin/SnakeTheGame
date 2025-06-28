b08924d9d710c8970d60b3cedd123e7173e7729d
Rilus <dellgin@yandex.ru>
2025-06-28 08:26:50 +0400

    Отрисовка еды и обновление отрисовки поля

---
+++ Food.cs
@@ -0,0 +1,14 @@
+﻿public class Food
+{
+    Point position;
+
+    public void Respawn()
+    {
+        position = new Point() { 
+            X = new Random().Next(1, 23) * 2,
+            Y = new Random().Next(1, 23)
+        };
+        Console.SetCursorPosition(position.X, position.Y);
+        Console.Write("*");
+    }
+}
--- Game.cs
+++ Game.cs
@@ -2,21 +2,24 @@
 {
     private static int _width = 48;
     private static int _height = 24;
+    private static Food food = new();
 
     public static void Run()
     {
         Console.CursorVisible = false;
         Console.Clear();
         RenderField();
+        food.Respawn();
+        Console.ReadKey();
     }
 
     private static void RenderField()
     {
-        Console.WriteLine(new string('#', _width));
+        Console.WriteLine(new string('#', _width - 1));
         for (int i = 1; i < _height - 1; i++)
         {
-            Console.WriteLine($"#{new string(' ', _width - 2)}#");
+            Console.WriteLine($"#{new string(' ', _width - 3)}#");
         }
-        Console.WriteLine(new string('#', _width));
+        Console.WriteLine(new string('#', _width - 1));
     }
 }

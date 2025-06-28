d37ab4d8e1c40fff941c2b19a51f5496619b4323
Rilus <dellgin@yandex.ru>
2025-06-28 09:24:51 +0400

    Добавление конструктора и обновление  спавна еды

--- Food.cs
+++ Food.cs
@@ -4,10 +4,7 @@
 
     public void Respawn()
     {
-        position = new Point() { 
-            X = new Random().Next(1, 23) * 2,
-            Y = new Random().Next(1, 23)
-        };
+        position = new Point(new Random().Next(1, 23) * 2, new Random().Next(1, 23));
         Console.SetCursorPosition(position.X, position.Y);
         Console.Write("*");
     }
--- Point.cs
+++ Point.cs
@@ -2,4 +2,9 @@
 {
     public int X;
     public int Y;
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
 }

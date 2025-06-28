323a852223cfcb9ee6c2a0a1eb8cbdf2825a587c
Rilus <dellgin@yandex.ru>
2025-06-28 11:46:05 +0400

    Добавление логики игры

--- Game.cs
+++ Game.cs
@@ -2,15 +2,45 @@
 {
     private static int _width = 48;
     private static int _height = 24;
-    private static Food food = new();
+    private static Snake _snake = new(new(22, 12));
+    private static Food _food = new(_snake);
 
     public static void Run()
     {
         Console.CursorVisible = false;
-        Console.Clear();
-        RenderField();
-        food.Respawn();
-        Console.ReadKey();
+        _food.Respawn();
+        while (true)
+        {
+            Console.Clear();
+            RenderField();
+            _snake.Draw();
+            _food.Draw();
+
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow: _snake.ChangeDirection(Direction.up); break;
+                    case ConsoleKey.DownArrow: _snake.ChangeDirection(Direction.down); break;
+                    case ConsoleKey.LeftArrow: _snake.ChangeDirection(Direction.left); break;
+                    case ConsoleKey.RightArrow: _snake.ChangeDirection(Direction.right); break;
+                }
+            }
+
+            _snake.Move();
+
+            if (_snake.CheckSelfCollision()) return;
+            if (_snake.CheckWallCollision(47, 24)) return;
+
+            if (_snake.Head.Position.Equals(_food.Position))
+            {
+                _snake.Grow();
+                _food.Respawn();
+            }
+
+            Thread.Sleep(500);
+        }
     }
 
     private static void RenderField()
